e36daa7d1e78febbe6371e7dfbba440aaedd49fd
sky7th <xoghk0321@gmail.com>
2020-04-22 18:04:11 +0900

    Add: 프로젝트 생성, h2 설정

---
+++ src/main/java/com/sky7th/springdatajpashop/SpringDataJpaShopApplication.java
@@ -0,0 +1,13 @@
+package com.sky7th.springdatajpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sky7th/springdatajpashop/SpringDataJpaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky7th.springdatajpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
